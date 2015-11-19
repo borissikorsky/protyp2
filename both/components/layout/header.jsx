a51c3a89e55dd668e1f9e7c174aff53bc15f28c1
Boris Sikorsky <sikorsky.kgstar@yandex.com>
2015-11-19 09:19:28 +0300

    have done navbar

--- both/components/layout/header.jsx
+++ both/components/layout/header.jsx
@@ -3,13 +3,12 @@ injectTapEventPlugin();
 let {
   FormsyText
 } = FMUI;
-
 var {
   AppBar,
   RaisedButton,
-  FloatingActionButton
+  FloatingActionButton,
+  LeftNav, MenuItem
 } = MUI;
-
 var {
   ThemeManager,
   LightRawTheme
@@ -77,15 +76,44 @@ Header = React.createClass({
   login: function () {
 
   },
+
+  toggleLeftNavbar: function (e) {
+    this.refs.leftNav.toggle();
+  },
+  toggleRightNavbar: function (e) {
+    this.refs.rightNav.toggle();
+  },
+
   render: function () {
     let {paperStyle, switchStyle, submitStyle } = this.styles;
     let { wordsError, urlError } = this.errorMessages;
 
-
+    leftMenuItems = [
+      { type: MenuItem.Types.SUBHEADER, text: 'This is left side bar' },
+      { route: 'get-started', text: 'First' },
+      { route: 'customization', text: 'Second' },
+      { route: 'components', text: 'Third' }
+    ];
+    rightMenuItems = [
+      { type: MenuItem.Types.SUBHEADER, text: 'This is right side bar' },
+      { route: 'get-started', text: 'First' },
+      { route: 'customization', text: 'Second' },
+      { route: 'components', text: 'Third' }
+    ];
     return (
       <div className="top-appbar">
         <AppBar
-          title="Protyp"
+          iconElementLeft={
+            <div className="header-left-area">
+              <FloatingActionButton
+                title="Register"
+                iconClassName="muidocs-icon-action-grade"
+                secondary={true}
+                mini={true}
+                onClick={this.toggleLeftNavbar}/>
+              <h3>Protyp</h3>
+            </div>
+          }
           iconElementRight={
             <div>
 
@@ -115,19 +143,36 @@ Header = React.createClass({
                   title="Log In"
                   type="submit"
                   iconClassName="muidocs-icon-action-grade"
-                  mini={true} /> &nbsp;
-
+                  mini={true} />&nbsp;
                 <FloatingActionButton
                   title="Register"
                   iconClassName="muidocs-icon-action-grade"
                   secondary={true}
                   linkButton={true}
                   href="/register"
+                  mini={true} />&nbsp;
+                <FloatingActionButton
+                  title="Show right nav"
+                  onClick={this.toggleRightNavbar}
+                  iconClassName="muidocs-icon-action-grade"
+                  secondary={true}
                   mini={true} />
               </Formsy.Form>
 
             </div>
           } />
+        <LeftNav
+          className="side-nav"
+          ref="leftNav"
+          docked={true}
+          menuItems={leftMenuItems} />
+
+        <LeftNav
+          className="side-nav"
+          ref="rightNav"
+          docked={true}
+          openRight={true}
+          menuItems={rightMenuItems} />
       </div>
     );
   }
